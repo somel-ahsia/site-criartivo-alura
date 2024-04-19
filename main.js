4130893afac160fe9cd799d928705419113e272c
somel-ahsia <106817883+somel-ahsia@users.noreply.github.com>
2024-04-19 09:49:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+contadores[0].textContent = tempoObjetivo1;
 
-        for(let j=0;j<botoes.length;j++){
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
